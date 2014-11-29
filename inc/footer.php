aa62f3389ac474e86ee3fa5410351c4669d1a43e
Hugo Rafael <hugor.souzalima@gmail.com>
2014-11-29 11:06:17 -0300

    Abas
    Alteração nos scripts e configuração básica

--- inc/footer.php
+++ inc/footer.php
@@ -11,5 +11,8 @@
 		</footer>
 	</div> <!-- END #container -->
 
+	<script src="./js/jquery-1.11.1.min.js" type="text/javascript"></script>
+	<script src="./js/scripts.js" type="text/javascript"></script>
+
 </body>
 </html>
---
+++ inc/todos.php
@@ -0,0 +1,25 @@
+<div id="todos" class="tab-content ativo">
+	<div class="grupo">
+		<article class="pequeno"></article>
+		<article class="pequeno"></article>
+	</div>
+	<article class="grande"></article>
+	<div class="grupo">
+		<article class="pequeno"></article>
+		<article class="pequeno"></article>
+	</div>
+	<article class="grande"></article>
+	<div class="grupo">
+		<article class="pequeno"></article>
+		<article class="pequeno"></article>
+	</div>
+	<div class="grupo">
+		<article class="pequeno"></article>
+		<article class="pequeno"></article>
+	</div>
+	<article class="grande"></article>
+	<div class="grupo">
+		<article class="pequeno"></article>
+		<article class="pequeno"></article>
+	</div>
+</div>
