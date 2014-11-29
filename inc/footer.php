c4bc8ec0dc112e692c5286c343cd9b95879e62d1
Hugo Rafael <hugor.souzalima@gmail.com>
2014-11-29 00:38:26 -0300

    Base
    Continuação da estrutura da base, praticamente finalizada.

---
+++ inc/footer.php
@@ -0,0 +1,15 @@
+		<footer>
+			<div class="wrap-footer">
+				<div class="left-side">
+					
+				</div>
+				<div class="right-side">
+					
+				</div>
+				<a class="link" href="http://saude.gov.br/partiuteste" target="_blank">saude.gov.br/partiuteste</a>
+			</div>
+		</footer>
+	</div> <!-- END #container -->
+
+</body>
+</html>
