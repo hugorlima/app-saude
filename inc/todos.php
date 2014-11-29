be1eb800cbcc0df4fbff50870cedf685709ba3a4
Hugo Rafael <hugor.souzalima@gmail.com>
2014-11-29 12:37:32 -0300

    base
    base

--- inc/todos.php
+++ inc/todos.php
@@ -1,6 +1,8 @@
 <div id="todos" class="tab-content ativo">
 	<div class="grupo">
-		<article class="pequeno"></article>
+		<article class="pequeno">
+			<span class="tag">download</span>
+		</article>
 		<article class="pequeno"></article>
 	</div>
 	<article class="grande"></article>
